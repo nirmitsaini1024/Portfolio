29eb0dcfac283a141bd97eccb18945378487e1fc
Nirmit Saini <nirmitsaini24@gmail.com>
2024-11-04 18:55:20 +0530

    optimization

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from "./components/navbar";
 import { Footer } from "./components/footer";
 
-// Import local fonts as before
 const geistSans = localFont({
   src: "./projects/fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -24,30 +23,15 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
         <link
           href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap"
           rel="stylesheet"
-          media="print"
-          onLoad={() => {
-            const link = document.querySelector(
-              'link[rel="stylesheet"][href*="Pacifico"]'
-            ) as HTMLLinkElement | null;
-            if (link) {
-              link.media = "all";
-            }
-          }}
         />
       </head>
       <body
