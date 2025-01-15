6eec8727ce64e295552967fcd248c4cb3f2bf2d9
nirmitsaini1024 <nirmitsaini24@gmail.com>
2025-01-15 19:58:53 +0530

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import HomeComponent from "./components/home";
-
+import dynamic from "next/dynamic";
+const LocomotiveProvider = dynamic(
+  () => import("./components/LocomotiveProvider"), // Import your hook's client context
+  { ssr: false } // Disable server-side rendering for this component
+);
 export default function Home() {
   return (
     <div>
-      <HomeComponent />
+      <LocomotiveProvider>
+        <HomeComponent />
+      </LocomotiveProvider>
     </div>
   );
 }
